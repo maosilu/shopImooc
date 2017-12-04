cf55cb1fa744073fc10b7e33645169668d255f55
Ice Guo <maosilu.ice@gmail.com>
2017-12-04 14:35:23 +0800

    bug修改

--- config/configs.php
+++ config/configs.php
@@ -6,8 +6,8 @@
  * Time: 上午10:13
  */
 //定义数据库的配置
-define(DB_HOST, 'localhost');
-define(DB_USER, 'root');
-define(DB_PWD, '123456');
-define(DB_DBNAME, 'shopImooc');
-define(DB_CHARSET, 'utf8');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PWD', '123456');
+define('DB_DBNAME', 'shopImooc');
+define('DB_CHARSET', 'utf8');
