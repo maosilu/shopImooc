cf62638e895a89bb53e3fc87227daecace284a5b
Ice Guo <maosilu.ice@gmail.com>
2017-12-04 18:11:32 +0800

    一周内自动登录功能

--- admin/doLogin.php
+++ admin/doLogin.php
@@ -9,11 +9,17 @@ require_once '../include.php';
 $username = htmlspecialchars($_POST['username']);
 $password = md5(htmlspecialchars($_POST['password']));
 $verify = htmlspecialchars($_POST['verify']);
+$autoFlag = $_POST['autoFlag'];
 $verify1 = $_SESSION['verifyCode'];
 if($verify == $verify1){
     $sql = "SELECT * FROM imooc_admin WHERE username='{$username}' AND password='{$password}'";
     $row = checkAdmin($sql);
     if($row){
+        //如果选择一周内自动登录
+        if($autoFlag){
+            setcookie('adminId', $row['id'], time()+7*24*3600);
+            setcookie('adminName', $row['username'], time()+7*24*3600);
+        }
         $_SESSION['adminName'] = $row['username'];
         $_SESSION['adminId'] = $row['id'];
         alertMsg('登录成功！', 'index.php');
--- core/admin.inc.php
+++ core/admin.inc.php
@@ -13,7 +13,7 @@ function checkAdmin($sql){
  */
 function checkLogined()
 {
-    if ($_SESSION['adminId'] == '') {
+    if (!isset($_SESSION['adminId']) && !isset($_COOKIE['adminId'])) {
         alertMsg('请先登录！', 'login.php');
     }
 }
@@ -24,6 +24,12 @@ function logout(){
     $_SESSION = array();
     if(isset($_COOKIE[session_name()])){
         setcookie(session_name(), '', time()-1);
+    }
+    if(isset($_COOKIE['adminId'])){
+        setcookie('adminId', '', time()-1);
+    }
+    if(isset($_COOKIE['adminName'])){
+        setcookie('adminName', '', time()-1);
     }
     session_destroy();
     header('location:login.php');
